ec56bae5d0a608c1218c08d43c4c37617f19a340
guusfelix2015 <gusfelix2015@gmail.com>
2024-09-25 20:30:26 -0300

    header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="pt">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} bg-zinc-950 text-zinc-50 antialiased`}
       >
         {children}
       </body>
