476cd12c1779183d5a9f89545ab3319f8cf1cda1
guusfelix2015 <gusfelix2015@gmail.com>
2024-09-26 22:29:03 -0300

    página de detalhes do produto

--- src/app/(store)/(home)/loading.tsx
+++ src/app/(store)/(home)/loading.tsx
@@ -4,8 +4,8 @@ export default function HomeLoading() {
   return (
     <div className="grid max-h-full grid-cols-9 grid-rows-6 gap-6">
       <Skeleton className="col-span-6 row-span-6 h-[768px]" />
-      <Skeleton className="col-span-3 row-span-3 h-[768px]" />
-      <Skeleton className="col-span-3 row-span-3 h-[768px]" />
+      <Skeleton className="col-span-3 row-span-3 h-[390px]" />
+      <Skeleton className="col-span-3 row-span-3 h-[380px]" />
     </div>
   )
 }
