2b06d4eb406b6df4da0e43e3dd604d41124331df
guusfelix2015 <gusfelix2015@gmail.com>
2024-09-28 16:26:04 -0300

    geração estática na build

--- src/app/(store)/product/[slug]/page.tsx
+++ src/app/(store)/product/[slug]/page.tsx
@@ -24,6 +24,12 @@ export async function generateMetadata({
   }
 }
 
+export async function generateStaticParams() {
+  const response = await api('/products/featured')
+  const products: Product[] = await response.json()
+  return products.map((product) => ({ slug: product.slug }))
+}
+
 export default async function ProductPage({
   params,
 }: {
